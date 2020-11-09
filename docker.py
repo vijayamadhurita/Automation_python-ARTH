a0ba5d37be4ad5f52b3b5381527145bea0f8ea0e
VIJAYA MADHURI T A <43950565+vijayamadhurita@users.noreply.github.com>
2020-11-09 16:04:51 +0530

    Add files via upload

---
+++ docker.py
@@ -0,0 +1,49 @@
+import os
+while(True):
+	print("Enter your choice ")
+	print("1: start docker services")
+	print("2: check the status of the docker services")
+	print("3: pull a docker image ")
+	print("4: check docker info ")
+	print("5: attach a docker image ")
+	print("6: install docker")
+	print("7: check the details of all the docker images running")
+	print("8: stop docker services")
+	print("9: search docker images ")
+	print("10: exit")
+
+
+	ch=int(input())
+
+	if(ch==1):
+		os.system("systemctl start docker")
+	elif(ch==2):
+		os.system("systemctl status docker")	
+	elif(ch==3):
+		name=input(" Enter the name of the docker image")
+		ver=input(" Enter the version")
+		os.system("docker pull {}:{}".format(name,ver))
+	elif(ch==4):
+		os.system("docker info")
+	elif (ch==5):
+		name=input(" Enter the name of the docker image to be attached")
+		os.system("docker start {}".format(name))		
+		os.system("docker attach {}".format(name))
+	elif(ch==6):
+		name=input(" Enter the image name")
+		ver=input(" Enter the version")
+		my_name=input(" Enter the name you want to assign to the docker container")
+		os.system("docker run -it --name {} {}:{}".format(my_name,name,ver))
+	elif(ch==7):
+		os.system("docker ps")
+	elif(ch==8):
+		os.system("systemctl stop docker")
+	elif(ch==9):
+		name=input(" Enter the docker image you want to search for ")
+		os.system("docker search {}".format(name))
+	elif(ch==10):
+		exit()
+	else:
+		print("Enter a valid choice")
+
+
---
+++ linux_cmd.py
@@ -0,0 +1,47 @@
+import os
+while True:
+	print("\t\tBASIC LINUX COMMANDS\t\t\t\n")
+	print("Enter your choice")
+	print("1: view the date")
+	print("2: view the calendar")
+	print("3: view the folders/files in the current directory")
+	print("4: create a directory")
+	print("5: add a user")
+	print("6: launch applications")
+	print("7: navigate to a directory ") 
+	print("8: check the current directoy u are in ")		
+	print("9: exit")
+	n=int(input())
+	if n==1:
+		print(os.system("date"))
+	elif n==2:
+		os.system("cal")
+	elif n==3:
+		os.system("ls")
+	elif n==4:
+		print(" Enter the directory name you want to create")
+		name=input()
+		os.system("mkdir {}".format(name))
+	elif n==5:
+		name=input(" Enter the username")
+		os.system("useradd {}".format(name))
+		print(" Enter the password")
+		os.system("passwd {}".format(name))
+	elif n==6:
+		print("Enter 1 to launch gedit")
+		print("Enter 2 to launch firefox")
+		ch=int(input())
+		if ch==1:
+			os.system("gedit &")
+		if ch==2:
+			os.system("firefox &")
+	elif n==7:
+		name=input("Enter the name of the directory you want to navigate to ")
+		os.system("cd /{}".format(name))
+	elif n==8:
+		os.system("pwd")
+	elif n==9:
+		exit()
+	else:
+		print(" Enter a valid choice !!")
+					
