bf8f2dcdf381e2d2340533df0f92fb71ec9098ed
Vikas Verma <vikasverma250999@gmail.com>
2020-11-10 19:55:04 +0530

    Update main.py

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import aws
 import docker
 import linux_cmd
 import lvm
+import webserver
 def mainf():
 	while True:
 		system("clear;tput setaf 5")
@@ -22,9 +23,10 @@ def mainf():
 		\t\t2. AWS
 		\t\t3. Docker
 		\t\t4. Linux Commands
-		\t\t5. LVM''')
+		\t\t5. LVM
+		\t\t6. WebServer''')
 		system("tput setaf 1")
-		print("\t\t\t\t6. EXIT")
+		print("\t\t\t\t7. EXIT")
 		system("tput setaf 5")
 		opt = input("\nEnter your choice: ")
 		system("tput setaf 2")
@@ -38,7 +40,9 @@ def mainf():
 			linux_cmd.cmd()
 		elif opt=='5':
 			lvm.lvm()
-		elif opt=='6':
+		elif opt=='5':
+			webserver.web()
+		elif opt=='7':
 			print("...Exited!")
 			system("tput setaf 7")
 			exit()
